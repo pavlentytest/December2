b56ed33df270016391da39b0d7778e53a795de75
Pvl <impetus@yandex.ru>
2019-12-04 19:02:51 +0300

    Добавил кнопку

--- app/src/main/java/com/samsung/myitschool/december2/MainActivity.java
+++ app/src/main/java/com/samsung/myitschool/december2/MainActivity.java
@@ -3,12 +3,21 @@ package com.samsung.myitschool.december2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // мега логика
     }
 }
