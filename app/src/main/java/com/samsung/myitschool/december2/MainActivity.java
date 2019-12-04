79a22b551d9d3810c9646e0a40446131540c0df0
holyhorde <matvei1203@gmail.com>
2019-12-04 19:05:52 +0300

    LOL

--- app/src/main/java/com/samsung/myitschool/december2/MainActivity.java
+++ app/src/main/java/com/samsung/myitschool/december2/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Павел Валерьевич - супер!
         // Я - второй
         // Ромка - лох 123
+        // MUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDA
     }
 
     @Override
